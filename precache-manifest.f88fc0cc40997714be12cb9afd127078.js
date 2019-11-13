fcd7aeb0d08e9620fc9bcbee9600c01d7bec08b1
Duong Tan Thanh <thanhduong12a2@gmail.com>
2019-11-13 10:11:24 +0700

    Updates

---
+++ precache-manifest.f88fc0cc40997714be12cb9afd127078.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bdc186ea435eaefad753efae3c20390",
+    "url": "/redux_cart/index.html"
+  },
+  {
+    "revision": "9f32e7d9c87d6379eda2",
+    "url": "/redux_cart/static/css/2.788c496a.chunk.css"
+  },
+  {
+    "revision": "a9ccdf86a2e78f5ed034",
+    "url": "/redux_cart/static/css/main.461a0194.chunk.css"
+  },
+  {
+    "revision": "9f32e7d9c87d6379eda2",
+    "url": "/redux_cart/static/js/2.9f757807.chunk.js"
+  },
+  {
+    "revision": "a9ccdf86a2e78f5ed034",
+    "url": "/redux_cart/static/js/main.3ef0be26.chunk.js"
+  },
+  {
+    "revision": "38af3f696c195a64029f",
+    "url": "/redux_cart/static/js/runtime-main.a2070894.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/redux_cart/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/redux_cart/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/redux_cart/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/redux_cart/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/redux_cart/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/redux_cart/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/redux_cart/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/redux_cart/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/redux_cart/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/redux_cart/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/redux_cart/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/redux_cart/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/redux_cart/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/redux_cart/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/redux_cart/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/redux_cart/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
